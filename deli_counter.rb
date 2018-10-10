b4e97f6ab89fe61df38eeace6eac65157b14f091
Sam B <s77broz@aol.com>
2018-10-10 15:13:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,18 @@
 # Write your code here.
 katz_deli = []
 
-def line(arr)
-  puts if arr.length == 0
+def line(deli)
+  puts if deli.length == 0
     "The line is currently empty."
   else
-    out = []
-    arr.each_with_index {|ele, i| out << "#{i + 1}. #{ele}"}
-    
-    "The line is currently: #{out.join(" ")}"
+    out = "The line is currently:"
+    i = 0
+    deli.length.times do 
+      out += " #{i + 1}. #{deli[i]}"
+      i += 1
+    end
+    out
+  end
 end
 
 def take_a_number(arr, name)
